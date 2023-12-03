c2bee082c8ccf96916daf4c8d342f9c7a3ce620d
pagwebgabriel <134543010+pagwebgabriel@users.noreply.github.com>
2023-12-03 11:41:33 -0300

    Add files via upload

---
+++ js/codijo.js
@@ -0,0 +1,7 @@
+var nombre=prompt("ingresa tu nombre:");
+var apellido=prompt("ingresa tu apellido");
+alert("hola "+nombre+" "+ apellido);
+
+function ponerenmayusculas()
+ var datospersonales=document.getElementById("datos");
+ datospersonales.innerHTML="datos personales";
